45ed3fa017514ea6d23a438bd46467a1542448ce
Alexey Belkov <lexabelco2013@yandex.ru>
2023-01-30 19:40:13 +0500

    feat: сверстал Login и Register

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,18 @@
 function Login() {
-  return (<>Login</>);
+  return (
+    <main>
+      <section className="auth">
+        <div className="auth__container">
+          <h2 className="auth__title">Вход</h2>
+          <form className="auth__form">
+            <input className="auth__input" placeholder="Email" />
+            <input className="auth__input" placeholder="Пароль" />
+            <button className="auth__btn-submit">Войти</button>
+          </form>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,19 @@
 function Register() {
-  return (<>Register</>);
+  return (
+    <main>
+      <section className="auth">
+        <div className="auth__container">
+          <h2 className="auth__title">Регистрация</h2>
+          <form className="auth__form">
+            <input className="auth__input" placeholder="Email" />
+            <input className="auth__input" placeholder="Пароль" />
+            <button className="auth__btn-submit">Зарегистрироваться</button>
+          </form>
+          <button className="auth__btn-footer">Уже зарегистрированы? Войти</button>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Register;
